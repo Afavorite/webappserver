63caa497f54a1cb6eb818134ed9003c975f7b4cb
AFavo <lai_kaiwen@qq.com>
2022-03-10 14:39:46 +0800

    OrderAdd功能初步完成

--- smartcabinet_server/src/com/demo/dao/OrderDAo.java
+++ smartcabinet_server/src/com/demo/dao/OrderDAo.java
@@ -1,4 +1,53 @@
 package com.demo.dao;
 
+import com.demo.bean.Order;
+import com.demo.utils.JdbcUtils;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class OrderDAo {
+    public Boolean orderadd(Order order){
+        boolean flag = false;
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = JdbcUtils.getconn();
+            String sql = "select box_status from box where box_number=?;";//查看计划预约的箱柜状态
+            preparedStatement = (PreparedStatement)connection.prepareStatement(sql);//组装sql语句
+            preparedStatement.setString(1,order.getOrder_box_number());
+            resultSet = preparedStatement.executeQuery();
+            if(resultSet.getString(1) == "free"){
+                flag = true;
+                sql = "update box set box_status = booked where box_number=?;";
+                preparedStatement = (PreparedStatement)connection.prepareStatement(sql);
+                preparedStatement.setString(1,order.getOrder_box_number());
+                preparedStatement.executeUpdate();
+                sql = "insert into orders(order_creator,order_box_number,order_status,order_use_start_time,order_use_end_time,order_temp,order_sterilization) values (?,?,?,?,?,?,?);";
+                preparedStatement = (PreparedStatement)connection.prepareStatement(sql);
+                preparedStatement.setString(1,order.getOrder_creator());
+                preparedStatement.setString(2,order.getOrder_box_number());
+                preparedStatement.setString(3,"booking");
+                preparedStatement.setString(4,order.getOrder_use_start_time());
+                preparedStatement.setString(5,order.getOrder_use_end_time());
+                preparedStatement.setString(6,order.getOrder_temp());
+                preparedStatement.setString(7,order.getOrder_sterilization());
+                preparedStatement.executeUpdate();
+                System.out.println("新建订单成功");
+            }
+            else{
+                System.out.println("创建订单失败，箱柜已被使用");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        finally{
+            JdbcUtils.close(preparedStatement,connection);
+        }
+        return flag;
+    }
 }
--- smartcabinet_server/src/com/demo/service/OrderAdd.java
+++ smartcabinet_server/src/com/demo/service/OrderAdd.java
@@ -1,6 +1,7 @@
 package com.demo.service;
 
 import com.demo.bean.Order;
+import com.demo.bean.User;
 import com.demo.dao.OrderDAo;
 
 import javax.servlet.ServletException;
@@ -8,15 +9,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Time;
 
 public class OrderAdd extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
         response.setContentType("text/html;charset=UTF-8");
-        //获取订单新建者，订单箱柜，订单设定温度，订单设定是否杀菌
+        //获取订单新建者，订单箱柜，使用开始时间，使用结束时间，订单设定温度，订单设定是否杀菌
         String order_creator = request.getParameter("order_creator");
         String order_box_number = request.getParameter("order_box_number");
+        String order_use_start_time = request.getParameter("order_use_start_time");
+        String order_use_end_time = request.getParameter("order_use_end_time");
         String order_temp = request.getParameter("order_temp");
         String order_sterilization = request.getParameter("order_sterilization");
 
@@ -25,5 +30,29 @@ public class OrderAdd extends HttpServlet {
         OrderDAo orderDAo = new OrderDAo();
 
         Order o = new Order();
+        o.setOrder_creator(order_creator);
+        o.setOrder_box_number(order_box_number);
+        o.setOrder_use_start_time(order_use_start_time);
+        o.setOrder_use_end_time(order_use_end_time);
+        o.setOrder_temp(order_temp);
+        o.setOrder_sterilization(order_sterilization);
+
+        boolean flag = orderDAo.orderadd(o);
+        PrintWriter out = response.getWriter();//回应请求
+        if(flag){
+            result = "success";
+        }
+        else {
+            result = "fail";
+        }
+        out.write(result);
+        out.flush();
+        out.close();
+        System.out.println(result);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
     }
 }
