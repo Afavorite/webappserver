828764bd9a3e02d33707f74a60069f919de45b11
AFavo <lai_kaiwen@qq.com>
2022-03-19 18:22:42 +0800

    实现箱柜数据的查询

--- smartcabinet_server/src/com/demo/dao/OrderDAo.java
+++ smartcabinet_server/src/com/demo/dao/OrderDAo.java
@@ -2,11 +2,11 @@ package com.demo.dao;
 
 import com.demo.bean.Order;
 import com.demo.utils.JdbcUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class OrderDAo {
     public Boolean orderadd(Order order){
@@ -49,5 +49,47 @@ public class OrderDAo {
             JdbcUtils.close(preparedStatement,connection);
         }
         return flag;
+    }
+
+    public String getboxinfo(String string){
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        // json数组
+        JSONArray array = new JSONArray();
+
+        try {
+            connection = JdbcUtils.getconn();
+            String sql = "select * from box where box_status=?;";//查看计划预约的箱柜状态
+            preparedStatement = (PreparedStatement)connection.prepareStatement(sql);//组装sql语句
+            preparedStatement.setString(1,"free");
+            resultSet = preparedStatement.executeQuery();
+            if(!resultSet.equals("")){
+
+                // 获取列数
+                ResultSetMetaData metaData = resultSet.getMetaData();
+                int columnCount = metaData.getColumnCount();
+
+                // 遍历ResultSet中的每条数据
+                while (resultSet.next()) {
+                    JSONObject jsonObj = new JSONObject();
+
+                    // 遍历每一列
+                    for (int i = 1; i <= columnCount; i++) {
+                        String columnName =metaData.getColumnLabel(i);
+                        String value = resultSet.getString(columnName);
+                        jsonObj.put(columnName, value);
+                    }
+                    array.put(jsonObj);
+                }
+            }
+
+        } catch (SQLException | JSONException e) {
+            e.printStackTrace();
+        }
+        finally{
+            JdbcUtils.close(preparedStatement,connection);
+        }
+        return array.toString();
     }
 }
--- smartcabinet_server/src/com/demo/service/GetBoxInfo.java
+++ smartcabinet_server/src/com/demo/service/GetBoxInfo.java
@@ -1,4 +1,37 @@
 package com.demo.service;
 
-public class GetBoxInfo {
+import com.demo.bean.Order;
+import com.demo.bean.User;
+import com.demo.dao.OrderDAo;
+import com.demo.dao.UserDAo;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class GetBoxInfo extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html;charset=UTF-8");
+        String result;
+        String i = req.getParameter("req");
+        OrderDAo orderDAo = new OrderDAo();
+
+        result = orderDAo.getboxinfo(i);
+        PrintWriter out = resp.getWriter();//回应请求
+
+        out.write(result);
+        out.flush();
+        out.close();
+        System.out.println(result);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
 }
